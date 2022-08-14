c53bc3f963aeb584655deb5b2aa78109b56b060f
“Gautam <gautamnath.in@gmail.com>
2022-08-14 13:31:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, options);
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+$(".sidenav a").on("click", function (e) {
+  // prevent default anchor click behavior
+  e.preventDefault();
+
+  // store hash
+  var hash = this.hash;
+
+  if ($(hash).length) {
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      1500,
+      function () {
+        // Do something fun if you want!
+      }
+    );
+  }
+});
+
+$("#enable-cards").on("click", function () {
+  $(this).hide(1000);
+  $(".card-section").show(1500);
+  $("html, body").animate(
+    {
+      scrollTop: $(".card-section").offset().top - 100,
+    },
+    2000
+  );
+});
